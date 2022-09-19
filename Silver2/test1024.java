6038400f9d375439481e332fb57c8a16fc7d565b
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2022-09-19 17:21:19 +0900

    update

---
+++ Silver2/test1024.java
@@ -0,0 +1,19 @@
+package Silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test1024 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int answer = 0;
+		
+	
+	}
+}
