5a1e62eed175b1eb17269ae2088aa961f6aa5db6
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2022-09-23 00:36:29 +0900

    update

--- Silver3/test2579.java
+++ Silver3/test2579.java
@@ -8,29 +8,19 @@ public class test2579 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int stair[] = new int[n+1];
-		int num = 0;
-		stair[0] = 0;
+		int stair[] = new int[n+2];
+		int dp[] = new int[n+2]; //�ִ밪
 		for(int i=1; i<=n; i++) {
 			stair[i] = Integer.parseInt(br.readLine());
 		}
+		dp[0]=0;
+		dp[1]=stair[1];
+		dp[2]=stair[1]+stair[2];
 		
-		int x = n;
-		while(x>0) {
-			if(stair[x-2]==0) {
-				num+=stair[x-1];
-				break;
-			}
-			
-			else if(stair[x-1]==0) {
-				break;
-			}
-			num += Math.max(stair[x-1], stair[x-2]);
-			if(x == stair[x-1]) {
-				x = x-1;
-			}
-			else
-				x = x-2;
+		for(int i=3; i<=n; i++) {
+			dp[i] = Math.max(dp[i-3]+stair[i-1], dp[i-2])+stair[i];
 		}
+		
+		System.out.println(dp[n]);
 	}
 }
