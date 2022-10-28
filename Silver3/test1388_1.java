910c5ba73af7a25faf1a1487974ab4c0fd74c682
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2022-10-29 03:18:35 +0900

    update

---
+++ Silver3/test1388_1.java
@@ -0,0 +1,45 @@
+package Silver3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test1388_1 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		String arr[][] = new String[N][M];
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i] = st.nextToken().split("");
+		}
+		
+		int cnt = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(arr[i][j].equals("-")) {
+					cnt++;
+					while(j<M && arr[i][j].equals("-"))
+						j++;
+				}
+			}
+		}
+		
+		for(int i=0; i<M; i++) {
+			for(int j=0; j<N; j++) {
+				if(arr[j][i].equals("|")) {
+					cnt++;
+					while(j<N && arr[j][i].equals("|"))
+						j++;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
