84ffa6c5702bd37222058d1ab97052fdbb3dfc78
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2022-10-29 03:18:14 +0900

    update

---
+++ Silver3/test17413.java
@@ -0,0 +1,46 @@
+package Silver3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class test17413 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		Stack <Character> stack = new Stack<>();
+		String str = br.readLine();
+		
+		boolean check = false;
+		
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i)=='<') {
+				check = true;
+				while(!stack.empty()) {
+					sb.append(stack.pop());
+				}
+				sb.append(str.charAt(i));
+			}
+			else if(str.charAt(i)=='>') {
+				check = false;
+				sb.append(str.charAt(i));
+			}
+			else if(check){
+				sb.append(str.charAt(i));
+			}
+			else {
+				if(str.charAt(i)==' ') {
+					while(!stack.empty())
+						sb.append(stack.pop());
+					sb.append(' ');
+				}
+				else
+					stack.add(str.charAt(i));
+			}
+		}
+		while(!stack.empty())
+			sb.append(stack.pop());
+		System.out.println(sb);
+	}
+}
