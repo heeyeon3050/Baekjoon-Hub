449ee083bba5c304cc5ad13d8ac5c61751e78651
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2022-11-02 20:56:47 +0900

    update

--- Silver1/test16194.java
+++ Silver1/test16194.java
@@ -1,32 +1,31 @@
 package Silver1;
 
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class test16194 {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int[] p = new int[1001]; // 카드팩 p[i] : i개 카드가 들어있는 카드팩.
-        int[] D = new int[10001]; //
-        for(int i = 1; i <= N; i++)
-            p[i] = sc.nextInt();
-
-        // 카드 1개부터 N개까지. ( Bottom-Up 방식 )
-        for(int i = 1; i <= N; i++){
-            D[i] = p[i]; // 최소값 초기화.
-            // d[i] = -1;
-            for(int j = 1; j <= i; j++){
-                D[i] = Math.min(D[i], D[i-j]+p[j]);
-                // if (d[i] == -1 || d[i] > d[i-j] + a[j]) {
-                //     d[i] = d[i-j] + a[j];
-                // }
-            }
-        }
-
-        System.out.println(D[N]);
-        sc.close();
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int dp[] = new int[N+1];
+		int cost[] = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			cost[i] = Integer.parseInt(st.nextToken());
+			dp[i] = cost[i];
+		}
+		
+		dp[0] = 0;
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=i; j++) {
+				dp[i] = Math.min(dp[i], dp[i-j]+cost[j]);
+			}
+		}
+		
+		System.out.println(dp[N]);
+	}
 }
