98aa48e4a0c61a990ab6b694f2b7e7fce4dc51ac
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2023-01-14 23:59:51 +0900

    update

---
+++ Bronze4/test2752.java
@@ -0,0 +1,28 @@
+package Bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class test2752 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int arr[] = new int[3];
+		
+		for(int i=0; i<3; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i=0; i<3; i++) {
+			sb.append(arr[i] + " ");
+		}
+		
+		System.out.println(sb);
+	}
+}
