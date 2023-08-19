073c3adaaa3d080418bc002b123ea54b8780dc99
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2023-08-19 23:13:49 +0900

    [Gold II] Title: 트리의 지름, Time: 820 ms, Memory: 86940 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -5,67 +5,68 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class Node{
-    int e;
-    int cost;
-
-    Node(int e, int cost){
-        this.e = e;
-        this.cost = cost;
-    }
-}
-
 public class Main {
-    static List<Node>[] list;
-    static boolean[] visit;
-    static int max = 0;
-    static int node;
+
+    static int v;
+    static List<int[]>[] adjList;
+    static boolean[] visited;
+    static int maxDist;
+    static int farNode;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int V = Integer.parseInt(br.readLine());
 
-        list = new ArrayList[V+1];
+        // 입력
+        v = Integer.parseInt(br.readLine());
+        adjList = new ArrayList[v+1];
+        visited = new boolean[v+1];
 
-        for(int i=1; i<=V; i++){
-            list[i] = new ArrayList<>();
+        for (int i = 1; i <= v; i++) {
+            adjList[i] = new ArrayList<>();
         }
 
-        for(int i=0; i<V; i++){
+
+        for (int i = 0; i < v; i++) {
             st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            while(true){
-                int e = Integer.parseInt(st.nextToken());
-                if(e == -1)
-                    break;
-                int cost = Integer.parseInt(st.nextToken());
-                list[num].add(new Node(e, cost));
+            int from = Integer.parseInt(st.nextToken());
+
+            while (true) {
+                int to = Integer.parseInt(st.nextToken());
+                if (to < 0) break;
+                int dist = Integer.parseInt(st.nextToken());
+
+                adjList[from].add(new int[]{to, dist});
+                adjList[to].add(new int[]{from, dist});
             }
         }
 
-        visit = new boolean[V+1];
+        // 루트부터 가장 먼 노드 찾기
         dfs(1, 0);
 
-        visit = new boolean[V+1];
-        dfs(node, 0);
+        int startNode = farNode;
 
-        System.out.println(max);
+        // 먼 노드부터, dfs
+        visited = new boolean[v+1];
+
+        dfs(startNode, 0);
+
+        System.out.println(maxDist);
     }
 
-    public static void dfs(int x, int len){
-        if(len > max){
-            max = len;
-            node = x;
+    private static void dfs(int cur, int dist) {
+        // visited action
+        visited[cur] = true;
+        if (dist > maxDist) {
+            maxDist = dist;
+            farNode = cur;
         }
 
-        visit[x] = true;
-
-        for(int i=0; i<list[x].size(); i++){
-            Node n = list[x].get(i);
-            if(!visit[n.e]){
-                dfs(n.e, len+n.cost);
-            }
+        for (int[] edge : adjList[cur]) {
+            int next = edge[0];
+            int cost = edge[1];
+            if (visited[next]) continue;
+            dfs(next, dist + cost);
         }
     }
 }
