856c026a5b25bbc695a1608c28cebb20191102fb
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2023-10-21 13:27:29 +0900

    [Gold IV] Title: 플로이드, Time: 464 ms, Memory: 44904 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n+1][n+1];
+
+		//초기값 설정
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n; j++){
+				arr[i][j] = 9900001;
+
+				if(i==j)
+					arr[i][j] = 0;
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			arr[a][b] = Math.min(arr[a][b], c);
+		}
+
+		for(int k=1; k<=n; k++){
+			for(int i=1; i<=n; i++){
+				for(int j=1; j<=n; j++){
+					if(arr[i][j] > arr[i][k] + arr[k][j]){
+						arr[i][j] = arr[i][k] + arr[k][j];
+					}
+				}
+			}
+		}
+
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n; j++){
+				if(arr[i][j] == 9900001)
+					arr[i][j] = 0;
+
+				sb.append(arr[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
