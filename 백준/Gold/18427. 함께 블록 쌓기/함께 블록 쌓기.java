9d3f0ec8e3f9056b41421bb4e062c9800420953c
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2023-12-27 13:37:51 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 176 ms, Memory: 17428 KB -BaekjoonHub

---
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+
+		int[][] dp = new int[N+1][1001];
+		List<Integer>[] list = new ArrayList[N+1];
+
+		for(int i=1; i<=N; i++){
+			list[i] = new ArrayList<>();
+			st = new StringTokenizer(br.readLine());
+			while(st.hasMoreTokens()){
+				list[i].add(Integer.parseInt(st.nextToken()));
+			}
+		}
+
+		for(int i=0; i<=N; i++){
+			dp[i][0] = 1;
+		}
+
+		for(int i=1; i<=N; i++){
+			for(int j=1; j<=H; j++){
+				dp[i][j] = dp[i-1][j];
+				for(Integer l : list[i]){
+					if(j >= l){
+						dp[i][j] += dp[i-1][j-l];
+						dp[i][j] %= 10007;
+					}
+				}
+			}
+		}
+
+		System.out.println(dp[N][H]);
+	}
+}
