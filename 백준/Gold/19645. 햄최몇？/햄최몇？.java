cccc449914cfd01637886691d7d36ae644a7e2ed
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-01-05 00:35:59 +0900

    [Gold I] Title: 햄최몇?, Time: 752 ms, Memory: 20900 KB -BaekjoonHub

--- 백준/Gold/19645. 햄최몇？/햄최몇？.java
+++ 백준/Gold/19645. 햄최몇？/햄최몇？.java
@@ -10,34 +10,32 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int[] a = new int[N+1];
+		int[] a = new int[N + 1];
 		int sum = 0;
 		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++){
+		for (int i = 1; i <= N; i++) {
 			a[i] = Integer.parseInt(st.nextToken());
 			sum += a[i];
 		}
 
-		boolean[][] dp = new boolean[sum+1][sum+1];
+		boolean[][] dp = new boolean[sum + 1][sum + 1];
 		dp[0][0] = true;
-		for(int i=1; i<=N; i++){
-			for(int x=sum; x>=0; x--){
-				for(int y=sum; y>=0; y--){
-					if(x + y <= sum) {
-						if (x - a[i] >= 0)
-							dp[x][y] |= dp[x - a[i]][y];
-						if (y - a[i] >= 0)
-							dp[x][y] |= dp[x][y - a[i]];
-					}
+		for (int i = 1; i <= N; i++) {
+			for (int x = sum; x >= 0; x--) {
+				for (int y = sum - x; y >= 0; y--) {
+					if (x - a[i] >= 0)
+						dp[x][y] |= dp[x - a[i]][y];
+					if (y - a[i] >= 0)
+						dp[x][y] |= dp[x][y - a[i]];
 				}
 			}
 		}
 
 		int answer = 0;
-		for(int i=0; i<=sum; i++){
-			for(int j=0; j<=i; j++){
+		for (int i = 0; i <= sum; i++) {
+			for (int j = 0; j <= i; j++) {
 				int last = sum - i - j;
-				if(dp[i][j] && (j >= last)){
+				if (dp[i][j] && (j >= last)) {
 					answer = Math.max(answer, last);
 				}
 			}
