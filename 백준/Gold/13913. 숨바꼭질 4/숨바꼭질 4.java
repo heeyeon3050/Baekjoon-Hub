503c08b0374f62a6b538556b99e75b7b8f912a39
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-01-22 17:52:51 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 360 ms, Memory: 37564 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int K;
+	static int arr[] = new int[100001];
+	static int parent[] = new int[100001];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		bfs(N);
+		sb.append(arr[K] + "\n");
+		
+		int num = K;
+		Stack<Integer> stack = new Stack<>();
+		for(int i=0; i<=arr[K]; i++) {
+			stack.add(num);
+			num = parent[num];
+		}
+		
+		for(int i=0; i<=arr[K]; i++) {
+			sb.append(stack.pop() + " ");
+		}
+		
+		System.out.println(sb);
+	}
+	public static void bfs(int x) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(x);
+		
+		while(!queue.isEmpty()) {
+			int now = queue.poll();
+			if(now == K)
+				return;
+			
+			if(now-1>=0 && arr[now-1]==0) {
+				arr[now-1] = arr[now] + 1;
+				parent[now-1] = now;
+				queue.add(now-1);
+			}
+			
+			if(now+1<=100000 && arr[now+1]==0) {
+				arr[now+1] = arr[now] + 1;
+				parent[now+1] = now;
+				queue.add(now+1);
+			}
+			
+			if(now*2<=100000 && arr[now*2]==0) {
+				arr[now*2] = arr[now] + 1;
+				parent[now*2] = now;
+				queue.add(now*2);
+			}
+		}
+	}
+}
