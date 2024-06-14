a6d6a329691ef5b4c97bd06509a2cd485ef8cd2c
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-06-14 20:22:59 +0900

    [Silver III] Title: 두 수의 합, Time: 400 ms, Memory: 31124 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -8,36 +8,37 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		int n = Integer.parseInt(br.readLine());
-		
-		Integer[] arr = new Integer[n];
-		
+
+		int[] arr = new int[n];
+
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
+		for(int i=0; i<n; i++){
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		int x = Integer.parseInt(br.readLine());
-		
+
+		Arrays.sort(arr);
+
 		int cnt = 0;
-		
-		Arrays.sort(arr, (o1, o2) -> Integer.compare(o1, o2));
-		
-		int left=0, right=n-1;
-        while(left < right) {
-            int sum = arr[left]+arr[right];
-            if(sum == x) {    // 같다면 count
-                cnt++;
-                left++;
-                right--;
-            }else if(sum > x) {    // 더 크다면
-                right--;
-            }else {    // 더 작다면
-                left++;
-            }
-        }
-		
+		int sum = 0;
+		int start = 0;
+		int end = n-1;
+
+		while(start < end){
+			sum = arr[start] + arr[end];
+
+			if(sum == x)
+				cnt++;
+
+			if(sum < x)
+				start++;
+			else
+				end--;
+		}
+
 		System.out.println(cnt);
 	}
 }
