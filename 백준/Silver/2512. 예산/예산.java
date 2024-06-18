163cdebda15de9c9d9719cf06a06afce9f900b8c
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-06-18 14:15:25 +0900

    [Silver II] Title: 예산, Time: 156 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -13,9 +13,11 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		arr = new int[N];
 		st = new StringTokenizer(br.readLine());
+        
 		for(int i=0 ; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
+        
 		int M = Integer.parseInt(br.readLine());
 		
 		Arrays.sort(arr);
