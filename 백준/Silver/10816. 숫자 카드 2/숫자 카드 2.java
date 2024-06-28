b67fd2f692171ceed67fa8772914d9b95e0f3fc2
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-06-29 06:14:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1220 ms, Memory: 120832 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,59 +5,65 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-        
-		int N = Integer.parseInt(br.readLine());
-		int a[] = new int[N];
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[n];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			a[i] = Integer.parseInt(st.nextToken());
+		for(int i=0; i<n; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-        
-		Arrays.sort(a);
-        
-		int M = Integer.parseInt(br.readLine());
+
+		Arrays.sort(arr);
+
+		int m = Integer.parseInt(br.readLine());
+
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) {
-			int key = Integer.parseInt(st.nextToken());
-			sb.append(upperBound(a, key) - lowerBound(a, key)).append(' ');
+		for(int i=0; i<m; i++){
+			int num = Integer.parseInt(st.nextToken());
+
+			int lower = lowerBound(arr, num);
+			int upper = upperBound(arr, num);
+
+			sb.append(upper-lower).append(" ");
 		}
-		
+
 		System.out.println(sb);
 	}
-	
-	static int lowerBound(int[] arr, int key) {
+
+	public static int lowerBound(int[] arr, int card){
 		int start = 0;
 		int end = arr.length;
-		
-		while(start < end) {
-			int mid = (start+end)/2;
-			
-			if(key <= arr[mid]) 
+
+		while(start < end){
+			int mid = (start + end) / 2;
+
+			if(arr[mid] >= card){
 				end = mid;
-			
-			else
+			} else
 				start = mid + 1;
 		}
-		return end;
+
+		return start;
 	}
-	
-	static int upperBound(int[] arr, int key) {
+
+	public static int upperBound(int[] arr, int card){
 		int start = 0;
 		int end = arr.length;
-		
-		while(start < end) {
-			int mid = (start+end) / 2;
-			
-			if(key < arr[mid])
+
+		while(start < end){
+			int mid = (start + end) / 2;
+
+			if(arr[mid] > card){
 				end = mid;
-			
-			else
+			} else
 				start = mid + 1;
 		}
-		return end;
+
+		return start;
 	}
 }
