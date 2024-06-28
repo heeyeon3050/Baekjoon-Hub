11afa6733b8a898ba916c8169e7126d622f90ce3
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-06-29 02:14:03 +0900

    [Silver I] Title: 회전 초밥, Time: 160 ms, Memory: 18580 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -20,8 +20,12 @@ public class Main {
 		for(int i=0; i<n; i++){
 			arr[i] = Integer.parseInt(br.readLine());
 		}
+		
+		for(int i=0; i<k; i++){
+			arr[n+i] = arr[i];
+		}
 
-		System.arraycopy(arr, 0, arr, arr.length-k, k);
+		//System.arraycopy(arr, 0, arr, arr.length-k, k);
 
 		int cnt = 0;
 		for(int i=0; i<k; i++){
