6ab902e16ae81f80be9d8804b279e8540c5669e0
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-07-11 05:09:54 +0900

    [Silver I] Title: 안정적인 문자열, Time: 136 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int num = 1;
+		while(true) {
+			String str = br.readLine();
+			if(str.charAt(0) == '-')
+				break;
+
+			int n = str.length();
+			int cnt = 0;
+
+			Stack<Character> stack = new Stack<>();
+
+			for(int i = 0; i < n; i++) {
+				char tmp = str.charAt(i);
+				if(tmp == '{') {
+					stack.add(tmp);
+				}else {
+					if(stack.isEmpty()) {
+						// '{'로 변경
+						cnt++;
+						stack.add('{');
+					}
+					else
+						stack.pop();
+				}
+			}
+
+			sb.append(num++).append(". ").append(cnt + stack.size()/2).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
