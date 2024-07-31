101ea3277b4536a63e670c2f42b5a1ac5c022445
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-07-31 23:21:24 +0900

    [Gold V] Title: 토마토, Time: 584 ms, Memory: 100624 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,82 +1,90 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int M, N, H;
-	static int map[][][];
-	static int dx[] = {0, 0, 1, -1, 0, 0};
-	static int dy[] = {1, -1, 0, 0, 0, 0};
-	static int dz[] = {0, 0, 0, 0, 1, -1};
-	static Queue<Point> queue = new LinkedList<>();
-	public static void main(String args[]) throws Exception {
+	static class Point{
+		int z, x, y;
+
+		public Point(int z, int x, int y){
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+
+	static int[] dx = {0, -1, 0, 1, 0, 0};
+	static int[] dy = {1, 0, -1, 0, 0, 0};
+	static int[] dz = {0, 0, 0, 0, 1, -1};
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		H = Integer.parseInt(st.nextToken());
-		
-		map = new int[H][N][M];
-		
-		for(int i=0; i<H; i++) {
-			for(int j=0; j<N; j++) {
+
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+
+		int[][][] arr = new int[h][n][m];
+		Queue<Point> queue = new ArrayDeque<>();
+
+		boolean flag = false;
+		for(int i=0; i<h; i++){
+			for(int j=0; j<n; j++){
 				st = new StringTokenizer(br.readLine());
-				for(int k=0; k<M; k++) {
-					map[i][j][k] = Integer.parseInt(st.nextToken());
-					
-					if(map[i][j][k]==1)
-						queue.offer(new Point(i, j, k));
+				for(int k=0; k<m; k++){
+					arr[i][j][k] = Integer.parseInt(st.nextToken());
+					if(arr[i][j][k] == 1){
+						queue.add(new Point(i, j, k));
+					} else {
+						flag = true;
 					}
-			}
-		}
-		bfs();
-		
-		int max = -1;
-		for(int i=0; i<H; i++) {
-			for(int j=0; j<N; j++) {
-				for(int k=0; k<M; k++) {
-					if(map[i][j][k] == 0) {
-						System.out.println(-1);
-						return;
-					}
-					max = Math.max(max, map[i][j][k]);
 				}
 			}
 		}
-		
-		System.out.println(max-1);
-	}
-	
-	public static void bfs() {
-		while(!queue.isEmpty()) {
-			Point now = queue.poll();
-			
-			for(int i=0; i<6; i++) {
-				int nz = now.z + dz[i];
-				int nx = now.x + dx[i];
-				int ny = now.y + dy[i];
-				
-				if(nx>=0 && ny>=0 && nz>=0 && nx<N && ny<M && nz<H) {
-					if(map[nz][nx][ny]==0) {
-						map[nz][nx][ny] = map[now.z][now.x][now.y] + 1;
+
+		if(!flag){
+			System.out.println(0);
+			return;
+		}
+
+		int day = 0;
+		while(!queue.isEmpty()){
+			int size = queue.size();
+			for(int i=0; i<size; i++){
+				Point now = queue.poll();
+
+				for(int j=0; j<6; j++){
+					int nx = now.x + dx[j];
+					int ny = now.y + dy[j];
+					int nz = now.z + dz[j];
+
+					if(nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h)
+						continue;
+
+					if(arr[nz][nx][ny] == 0) {
+						arr[nz][nx][ny] = 1;
 						queue.add(new Point(nz, nx, ny));
 					}
 				}
 			}
+			day++;
 		}
-	}
-}
 
-class Point{
-	int z;
-	int x;
-	int y;
-	
-	Point(int z, int x, int y){
-		this.z = z;
-		this.x = x;
-		this.y = y;
+		for(int i=0; i<h; i++){
+			for(int j=0; j<n; j++){
+				for(int k=0; k<m; k++){
+					if(arr[i][j][k] == 0){
+						System.out.println(-1);
+						return;
+					}
+				}
+			}
+		}
+
+		System.out.println(day-1);
 	}
 }
