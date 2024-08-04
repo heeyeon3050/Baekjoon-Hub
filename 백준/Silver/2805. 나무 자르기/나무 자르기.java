d3f2a41848108d04bc981e780971aaf8237836ed
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-08-05 05:29:09 +0900

    [Silver II] Title: 나무 자르기, Time: 912 ms, Memory: 124576 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -10,39 +10,45 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		long m = Long.parseLong(st.nextToken());
 
-		int[] arr = new int[n];
+		long[] arr = new long[n];
+
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++){
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Long.parseLong(st.nextToken());
 		}
 
 		Arrays.sort(arr);
 
-		int start = 0;
-		int end = arr[arr.length-1];
-
-		while(start < end){
-			int mid = (start + end) / 2;
-
-			if(func(arr, n, mid) < m)
-				end = mid;
-			else {
-				start = mid + 1;
-			}
-		}
-
-		System.out.println(start-1);
+		System.out.println(upperBound(arr, m)-1);
 	}
 
-	public static long func(int[] arr, int n, int mid){
+	public static long upperBound(long[] arr, long m){
+		long start = 0;
+		long end = arr[arr.length-1];
+
+		while(start < end){
+			long mid = (start + end) / 2;
+
+			if(height(arr, mid) >= m){
+                start = mid + 1;
+			} else
+				end = mid;
+		}
+
+		return start;
+	}
+
+	public static long height(long[] arr, long mid){
+		long n = arr.length;
+
 		long sum = 0;
 		for(int i=0; i<n; i++){
-			if(arr[i] > mid){
+			if(arr[i] > mid)
 				sum += arr[i] - mid;
-			}
 		}
+
 		return sum;
 	}
 }
