ecf029104fcdd0b7d82722bea7f04fb28f269ef2
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-08-05 04:11:59 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 120 ms, Memory: 15484 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,105 +1,94 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- *  열쇠를 획득했을 경우에 대한 방문 기록을 가져야 함. -> 3차원 배열
- */
-
-class Node {
-	int row;
-	int col;
-	int cnt; //이동 횟수
-	int key; //가지고 있는 열쇠 정보
-
-	public Node(int row, int col, int cnt, int key) {
-		this.row = row;
-		this.col = col;
-		this.cnt = cnt;
-		this.key = key;
-	}
-}
-
 public class Main {
+	static class Node {
+		int x;
+		int y;
+		int cnt; //이동 횟수
+		int key; //가지고 있는 열쇠 정보
+
+		public Node(int x, int y, int cnt, int key) {
+			this.x = x;
+			this.y = y;
+			this.cnt = cnt;
+			this.key = key;
+		}
+	}
+
 	public static int[] dx = new int[] { 0, 0, -1, 1 };
 	public static int[] dy = new int[] { 1, -1, 0, 0 };
-	public static char[][] map;
-	public static boolean[][][] visited;
+	public static char[][] arr;
+	public static boolean[][][] visit;
+	static int n, m;
 	public static Node start;
-	public static int N, M, ans = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new char[N][M];
-		visited = new boolean[64][N][M]; //열쇠는 최대 6개 이므로 2^6=64
 
-		for (int i = 0; i < N; i++) {
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		arr = new char[n][m];
+		visit = new boolean[64][n][m]; //열쇠가 6개이므로 2^6 = 64
+
+		for(int i=0; i<n; i++){
 			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				map[i][j] = str.charAt(j);
-				if (map[i][j] == '0') //시작 위치 설정
+			for(int j=0; j<m; j++){
+				arr[i][j] = str.charAt(j);
+				if(arr[i][j] == '0')
 					start = new Node(i, j, 0, 0);
 			}
 		}
+
 		System.out.println(bfs());
 	}
 
-	public static int bfs() {
-		Queue<Node> q = new LinkedList<Node>();
-		q.offer(new Node(start.row, start.col, 0, 0));
-		visited[0][start.row][start.col] = true;
+	public static int bfs(){
+		Queue<Node> queue = new ArrayDeque<>();
+		queue.add(start);
+		visit[0][start.x][start.y] = true;
 
-		while (!q.isEmpty()) {
-			Node node = q.poll();
-			int row = node.row;
-			int col = node.col;
-			int cnt = node.cnt;
-			int key = node.key;
+		while(!queue.isEmpty()){
+				Node now = queue.poll();
+			for(int j=0; j<4; j++){
+				int nx = now.x + dx[j];
+				int ny = now.y + dy[j];
 
-			if (map[row][col] == '1') { //만약 해당 위치가 출구라면 이동 횟수 반환
-				return cnt;
-			}
+				if(nx < 0 || nx >= n || ny < 0 || ny >= m || visit[now.key][nx][ny])
+					continue;
 
-			for (int i = 0; i < 4; i++) {
-				int nr = row + dx[i];
-				int nc = col + dy[i];
+				if(arr[nx][ny] == '#')
+					continue;
 
-				//경계 체크, 방문 체크, 벽이 아닌 경우
-				if (isBoundary(nr, nc) && map[nr][nc] != '#' && !visited[key][nr][nc]) {
-					if (map[nr][nc] == '.' || map[nr][nc] == '0' || map[nr][nc] == '1') {
-						visited[key][nr][nc] = true;
-						q.offer(new Node(nr, nc, cnt + 1, key));
+				if(arr[nx][ny] == '1')
+					return now.cnt+1;
 
-					} else if (map[nr][nc] >= 'a' && map[nr][nc] <= 'z') {
-						int newKey = 1 << (map[nr][nc] - 'a'); //열쇠일 경우 2의 제곱
-						newKey = newKey | key;
-						if (!visited[newKey][nr][nc]) {
-							visited[key][nr][nc] = true;
-							visited[newKey][nr][nc] = true;
-							q.offer(new Node(nr, nc, cnt + 1, newKey));
-						}
-
-					} else if (map[nr][nc] >= 'A' && map[nr][nc] <= 'Z') {
-
-						int door = 1 << (map[nr][nc] - 'A');
-						if ((key & door) > 0) { // 값이 같다면. 해당 문을 열 수 있는 열쇠가 있다면.
-							visited[key][nr][nc] = true;
-							q.offer(new Node(nr, nc, cnt + 1, key));
-						}
+				if(arr[nx][ny] == '.' || arr[nx][ny] == '0'){
+					visit[now.key][nx][ny] = true;
+					queue.offer(new Node(nx, ny, now.cnt+1, now.key));
+				} else if(arr[nx][ny] >= 'a' && arr[nx][ny] <= 'f'){
+					int newKey = 1 << (arr[nx][ny] - 'a'); //2의 제곱승
+					newKey = newKey | now.key;
+					if(!visit[newKey][nx][ny]){
+						visit[now.key][nx][ny] = true;
+						visit[newKey][nx][ny] = true;
+						queue.offer(new Node(nx, ny, now.cnt+1, newKey));
+					}
+				} else if(arr[nx][ny] >= 'A' && arr[nx][ny] <= 'F'){
+					int door = 1 << (arr[nx][ny] - 'A');
+					if((now.key & door) > 0){
+						visit[now.key][nx][ny] = true;
+						queue.offer(new Node(nx, ny, now.cnt+1, now.key));
 					}
 				}
 			}
 		}
 		return -1;
 	}
-
-	public static boolean isBoundary(int row, int col) {
-		return (row >= 0 && row < N) && (col >= 0 && col < M);
-	}
-
 }
