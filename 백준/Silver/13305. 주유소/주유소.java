f9a3040deedbe1f15ae75cdc10c0e23e4617c9ec
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-15 23:55:10 +0900

    [Silver III] Title: 주유소, Time: 384 ms, Memory: 35380 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int dis[] = new int[N-1];
+		int cost[] = new int[N-1];
+		long sum = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N-1; i++) {
+				dis[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N-1; i++) {
+			cost[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		long minCost = cost[0];
+		for(int i=0; i<N-1; i++) {
+			if(cost[i]<minCost) {
+				minCost = cost[i];
+			}
+			sum += dis[i]*minCost;
+		}
+		
+		System.out.println(sum);
+	}
+}
