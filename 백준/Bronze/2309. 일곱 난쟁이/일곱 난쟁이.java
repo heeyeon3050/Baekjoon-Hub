f9424be979abc75be6a29e8049c4eac52d31e7d1
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-16 22:32:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 96 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,34 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int arr[] = new int[9];
-	static boolean visit[] = new boolean[9];
-	static StringBuilder sb = new StringBuilder();
-    
-	public static void main(String args[]) throws Exception{
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
+
+		arr = new int[9];
+
 		int sum = 0;
-		for(int i=0; i<9; i++) {
+		for(int i=0; i<9; i++){
 			arr[i] = Integer.parseInt(br.readLine());
 			sum += arr[i];
 		}
-		
-		for(int i=0; i<8; i++) {
-			for(int j=i+1; j<9; j++) {
-				if(sum - arr[i] - arr[j] == 100) {
-					arr[i] = 0;
-					arr[j] = 0;
-					Arrays.sort(arr);
+
+		outerLoop:
+		for(int i=0; i<8; i++){
+			for(int j=i+1; j<9; j++){
+				if(sum - arr[i] - arr[j] == 100){
+					arr[i] = 101;
+					arr[j] = 101;
 					
-					for(int k=2; k<9; k++) {
-						System.out.println(arr[k]);
-					}
-					return;
+					break outerLoop;
 				}
 			}
 		}
+
+		Arrays.sort(arr);
+
+		for(int i=0; i<7; i++){
+			sb.append(arr[i]).append("\n");
+		}
+
+		System.out.println(sb);
 	}
 }
