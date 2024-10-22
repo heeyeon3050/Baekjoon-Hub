9b4b08e5f96ffdd24555b8005d83d19ef2912924
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-22 22:38:29 +0900

    [Gold IV] Title: 오큰수, Time: 912 ms, Memory: 148016 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] nge = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Stack<Integer> stack = new Stack<>();
+		stack.add(0);
+
+		for(int i=0; i<n; i++){
+			while(!stack.isEmpty() && arr[i] > arr[stack.peek()]){
+				nge[stack.pop()] = arr[i];
+			}
+			stack.push(i);
+		}
+
+		for(int i=0; i<n; i++){
+			if(nge[i] == 0)
+				sb.append(-1);
+			else
+				sb.append(nge[i]);
+			sb.append(" ");
+		}
+
+		System.out.println(sb);
+	}
+}
