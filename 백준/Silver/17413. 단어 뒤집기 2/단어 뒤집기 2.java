3d51b39c96f5055651acb13c03bc46e44900690c
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-22 20:46:44 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 216 ms, Memory: 22920 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -11,34 +11,31 @@ public class Main {
 		int length = str.length();
 		boolean flag = false;
 		Stack<Character> stack = new Stack<>();
-		Queue<Character> queue = new ArrayDeque<>();
 		for(int i=0; i<length; i++){
 			char x = str.charAt(i);
-			if(flag && x == '>'){
-				queue.add(x);
-				while(!queue.isEmpty()){
-					sb.append(queue.poll());
-				}
-				flag = false;
-				queue = new ArrayDeque<>();
-			} else if(flag && (x >= 'a' && x <= 'z' || x == ' ')){
-				queue.add(x);
-			} else if(!flag && ((x >= 'a' && x <= 'z') || (x >= '0' && x<= '9'))){
-				stack.push(x);
-			} else if(x == '<') {
-				flag = true;
-				queue.add(x);
-				while(!stack.isEmpty()){
-					sb.append(stack.pop());
-				}
-				stack = new Stack<>();
-			} else if(!flag && x == ' '){
 
+			if(x == '<'){
+				flag = true;
 				while(!stack.isEmpty()){
 					sb.append(stack.pop());
 				}
-				sb.append(" ");
-				stack = new Stack<>();
+			} else if(x == '>'){
+				flag = false;
+				sb.append(x);
+				continue;
+			}
+
+			if(flag)
+				sb.append(x);
+			else {
+				if(x == ' ') {
+					while (!stack.isEmpty()) {
+						sb.append(stack.pop());
+					}
+					sb.append(" ");
+				} else {
+					stack.push(x);
+				}
 			}
 		}
 
