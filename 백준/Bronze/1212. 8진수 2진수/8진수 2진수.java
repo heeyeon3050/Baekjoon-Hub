e47d0f53bf140b839c597560e3d333a4dbb2e546
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-24 20:47:49 +0900

    [Bronze II] Title: 8진수 2진수, Time: 292 ms, Memory: 26012 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -1,26 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		// 방법 1
+		// String str = br.readLine();
+		// BigInteger OctalToDecimal = new BigInteger(str, 8);
+		// System.out.println(OctalToDecimal.toString(2));
 
-        String str = br.readLine();
-        String[] arr = {"000", "001", "010", "011", "100", "101", "110", "111"};
+		String[] arr = {"000","001","010","011","100","101","110","111"};
+		String str = br.readLine();
+		int length = str.length();
 
-        for(int i=0; i<str.length(); i++){
-            int a = str.charAt(i) -'0';
-            sb.append(arr[a]);
-        }
+		for(int i=0; i<length; i++){
+			sb.append(arr[str.charAt(i)-'0']);
+		}
 
-        if(str.equals("0"))
-            System.out.println(str);
-        else {
-            while (sb.charAt(0) == '0')
-                sb = new StringBuilder(sb.substring(1));
-            System.out.println(sb);
-        }
-    }
+		if(str.equals("0"))
+			System.out.println(str);
+		else{
+			while(sb.charAt(0) == '0')
+				sb = new StringBuilder(sb.substring(1));
+			System.out.println(sb);
+		}
+	}
 }
