490686b83369a1d2feacca1345875fc5239e9846
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-24 21:42:17 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 264 ms, Memory: 18824 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,34 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static boolean prime[] = new boolean[1000001];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        func();
+	static final int INF = 1000000;
+	static boolean[] prime = new boolean[INF+1];
 
-        int T = Integer.parseInt(br.readLine());
-        for(int t=0; t<T; t++){
-            int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-            int cnt = 0;
-            for(int i=2; i<=N/2; i++){
-                if(!prime[i] && !prime[N-i]){
-                    cnt++;
-                }
-            }
-            sb.append(cnt + "\n");
-        }
-        System.out.println(sb);
-    }
+		Arrays.fill(prime, true);
+		isPrime();
 
-    public static void func(){
-        for(int i=2; i<=1000000/2; i++){
-            for(int j=i*2; j<=1000000; j+=i){
-                prime[j] = true; //소수 아님
-            }
-        }
-    }
+		int t = Integer.parseInt(br.readLine());
+		for(int tc=0; tc<t; tc++){
+			int n = Integer.parseInt(br.readLine());
+
+			int cnt = 0;
+			for(int i=2; i<=n/2; i++){
+				if(prime[i] && prime[n-i])
+					cnt++;
+			}
+
+			sb.append(cnt).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	public static void isPrime(){
+		prime[0] = false;
+		prime[1] = false;
+
+		for(int i=2; i*i<=INF; i++){
+			if(prime[i]){
+				for(int j=i*i; j<=INF; j+=i){
+					prime[j] = false;
+				}
+			}
+		}
+	}
 }
