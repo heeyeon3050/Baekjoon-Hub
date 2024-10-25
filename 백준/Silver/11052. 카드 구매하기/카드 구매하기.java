3373633b569ed20ad445784ae39ebd7a39c37acf
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-25 21:57:08 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14564 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=n; i++){
+			dp[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=i/2; j++){
+				dp[i] = Math.max(dp[i], dp[i-j] + dp[j]);
+			}
+		}
+
+		System.out.println(dp[n]);
+	}
+}
