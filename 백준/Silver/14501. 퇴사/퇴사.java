90b27c471ebc1856554a334d7c13de4c2db54786
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-26 02:04:28 +0900

    [Silver III] Title: 퇴사, Time: 112 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,41 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] t;
-    static int[] p;
-    static int n;
-    static int max = Integer.MIN_VALUE;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int[] t = new int[n+1];
+		int[] p = new int[n+1];
 
-        t = new int[n+1];
-        p = new int[n+1];
-        for(int i=1; i<=n; i++){
-            st = new StringTokenizer(br.readLine());
-            t[i] = Integer.parseInt(st.nextToken());
-            p[i] = Integer.parseInt(st.nextToken());
-        }
+		for(int i=1; i<=n; i++){
+			st = new StringTokenizer(br.readLine());
+			t[i] = Integer.parseInt(st.nextToken());
+			p[i] = Integer.parseInt(st.nextToken());
+		}
 
-        dfs(1, 0);
-        System.out.println(max);
-    }
+		int[] dp = new int[n+2];
+		for(int i=1; i<=n; i++){
+			if (i+t[i] <= n+1) // 일 했을 경우
+				dp[i+t[i]] = Math.max(dp[i+t[i]], dp[i]+p[i]);
+			//오늘 일한 값을 다음 날로 누적 (일 안했을 경우)
+			dp[i+1] = Math.max(dp[i+1], dp[i]);
+		}
 
-    public static void dfs(int L, int sum){
-        if(L > n){
-            max = Math.max(max, sum);
-            return;
-        }
-
-        if(L + t[L] <= n+1)
-            dfs(L+t[L], sum + p[L]);
-
-        if(L+1 <= n+1)
-            dfs(L+1, sum);
-    }
+		System.out.println(dp[n+1]);
+	}
 }
