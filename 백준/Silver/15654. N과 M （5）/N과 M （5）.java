e5304bf1aa0574e8f9815a916040875b35f4c772
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-26 21:30:24 +0900

    [Silver III] Title: N과 M (5), Time: 308 ms, Memory: 29724 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,48 +1,47 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int arr[];
-	static int num[];
-	static boolean visit[];
+	static int n, m;
+	public static int[] num;
+	public static int[] arr;
+	public static boolean[] visit;
 	static StringBuilder sb = new StringBuilder();
-	public static void main(String args[]) throws Exception{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		num = new int[M];
-		visit = new boolean[N];
-		
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		num = new int[n];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(arr);
-		dfs(0);
-		
+		for(int i=0; i<n; i++){
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(num);
+
+		arr = new int[m];
+		visit = new boolean[n];
+		recur(0);
+
 		System.out.println(sb);
 	}
-	
-	public static void dfs(int cnt) {
-		if(cnt == M) {
-			for(int n : num) {
-				sb.append(n + " ");
-			}
+
+	public static void recur(int depth){
+		if(depth == m){
+			for(int a : arr)
+				sb.append(a).append(" ");
 			sb.append("\n");
 			return;
 		}
-		
-		for(int i=0; i<N; i++) {
+
+		for(int i=0; i<n; i++){
 			if(!visit[i]) {
+				arr[depth] = num[i];
 				visit[i] = true;
-				num[cnt] = arr[i];
-				dfs(cnt+1);
+				recur(depth + 1);
 				visit[i] = false;
 			}
 		}
