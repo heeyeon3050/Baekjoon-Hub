2b96b8713252f1346d2101191e3037a7bc87442f
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-26 22:52:00 +0900

    [Silver I] Title: NM과 K (1), Time: 196 ms, Memory: 16080 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -1,65 +1,66 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M, K;
-	static int arr[][];
-	static boolean visit[][];
-	static int dx[] = {0, 0, 1, -1};
-	static int dy[] = {1, -1, 0, 0};
-	static int sum = 0;
-	static boolean flag = true;
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, -1, 0, 1};
+	static int n, m, k;
+	public static int[] num;
+	public static int[][] arr;
+	public static boolean[][] visit;
 	static int max = Integer.MIN_VALUE;
-	public static void main(String args[]) throws Exception{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N][M];
-		visit = new boolean[N][M];
-		
-		for(int i=0; i<N; i++) {
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		arr = new int[n][m];
+		visit = new boolean[n][m];
+		for(int i=0; i<n; i++){
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<M; j++) {
+			for(int j=0; j<m; j++){
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		dfs(0, 0, 0, 0);
+
+		recur(0, 0, 0, 0);
+
 		System.out.println(max);
 	}
-	
-	public static void dfs(int x, int y, int cnt, int sum) {
-		if(cnt == K) {
+
+	public static void recur(int cnt, int x, int y, int sum){
+		if(cnt == k){
 			max = Math.max(max, sum);
 			return;
 		}
-		
-		for(int i=x; i<N; i++) {
-			for(int j=y; j<M; j++) {
-				if(!visit[i][j]) {
-					flag = true;
-					for(int k=0; k<4; k++) {
-						int nx = i + dx[k];
-						int ny = j + dy[k];
-						
-						if(nx>=0 && ny>=0 && nx<N && ny<M) {
-							if(visit[nx][ny]) {
-								flag = false;
-								break;
-							}
-						}
-					}
-					if(flag) {
-						visit[i][j] = true;
-						dfs(x, y, cnt+1, sum + arr[i][j]);
-						visit[i][j] = false;
-					}
+
+		for(int i=x; i<n; i++){
+			for(int j = (i == x ? y : 0); j < m; j++){
+				if(visit[i][j])
+					continue;
+				if(check(i, j)){ //인접한 값이 없을 경우
+					visit[i][j] = true;
+					recur(cnt+1, i, j+1, sum+arr[i][j]);
+					visit[i][j] = false;
 				}
 			}
 		}
+	}
+
+	public static boolean check(int x, int y){
+		for(int i=0; i<4; i++){
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if(nx < 0 || nx >= n || ny < 0 || ny >= m)
+				continue;
+
+			if(visit[nx][ny])
+				return false;
+		}
+		return true;
 	}
 }
