91dc79d2f9aeaaa37dd8c0266a70cfe48610fe01
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-27 00:32:18 +0900

    [Silver II] Title: 예산, Time: 148 ms, Memory: 15196 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,47 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int arr[];
-	
-	public static void main(String[] args) throws IOException{
+	static int n, m;
+	static int[] arr;
+	static int max = 0;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		arr = new int[N];
+
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
 		st = new StringTokenizer(br.readLine());
-        
-		for(int i=0 ; i<N; i++) {
+		for(int i=0; i<n; i++){
 			arr[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, arr[i]);
 		}
-        
-		int M = Integer.parseInt(br.readLine());
-		
-		Arrays.sort(arr);
-		
+		m = Integer.parseInt(br.readLine());
+
+		System.out.println(upperBound()-1);
+	}
+
+	public static int upperBound(){
 		int start = 0;
-		int end = arr[N-1];
-		int answer = 0;
-		while(start <= end) {
-			int mid = (start + end) / 2; //내야할 세금
-			long budget = 0; //모든 지방 세금 합
-			for(int i=0; i<N; i++) {
-				if(arr[i] >= mid) 
-					budget += mid; //요청한 예산액이 더 크면 상한액만 배정
-				else
-					budget += arr[i]; //요청한 예산액보다 작으면 해당 값만 배정
-			}
-			if(budget > M) //다 배정한 값이 국가예산보다 넘으면 내야할 세금을 다시 수정
-				end = mid - 1;
-			else {
-				start = mid + 1; //작으면 범위를 크게해서 다시 계산
-				answer = Math.max(answer, mid); 
+		int end = max + 1;
+
+		while(start < end){
+			int mid = (start+end) / 2;
+
+			if(func(mid) <= m){
+				start = mid+1;
+			}else {
+				end = mid;
 			}
 		}
-		
-		System.out.println(answer);
+		return start;
+	}
+
+	public static long func(int mid){
+		long sum = 0;
+		for(int i=0; i<n; i++){
+			if(arr[i] > mid){
+				sum += mid;
+			} else {
+				sum += arr[i];
+			}
+		}
+		return sum;
 	}
 }
