2e85266cc6c93039064157ffcf0cc9914a664748
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-27 00:05:02 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 328 ms, Memory: 32876 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,37 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[N+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
 		int[] cnt = new int[100001];
-		
+
+		int[] arr = new int[n];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<n; i++){
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		int left = 0, right = 0;
-		int max = Integer.MIN_VALUE;
-		while(right < N) {
-			cnt[arr[right]]++;
-			while(cnt[arr[right]] > K) {
-				cnt[arr[left++]]--;
+
+		int start = 0;
+		int end = 0;
+		int max = 0;
+		while(end < n){
+			if(cnt[arr[end]] < k){
+				cnt[arr[end]]++;
+				end++;
+			} else {
+				cnt[arr[start]]--;
+				start++;
 			}
-			
-			int result = right - left + 1;
-			max = Math.max(max, result);
-			right++;
+			max = Math.max(max, end - start);
 		}
+
 		System.out.println(max);
 	}
 }
