0ac75b813c3c0679577fc7e3fcfc10114655f5ca
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-26 21:52:39 +0900

    [Silver II] Title: N과 M (9), Time: 180 ms, Memory: 21352 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, m;
+	public static int[] num;
+	public static int[] arr;
+	public static boolean[] visit;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		num = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++){
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(num);
+
+		arr = new int[m];
+		visit = new boolean[n];
+		recur(0);
+
+		System.out.println(sb);
+	}
+
+	public static void recur(int depth){
+		if(depth == m){
+			for(int a : arr)
+				sb.append(a).append(" ");
+			sb.append("\n");
+			return;
+		}
+
+		int prev = 0;
+		for(int i=0; i<n; i++){
+			if(!visit[i] && prev != num[i]) {
+				arr[depth] = num[i];
+				visit[i] = true;
+				prev = num[i];
+				recur(depth + 1);
+				visit[i] = false;
+			}
+		}
+	}
+}
