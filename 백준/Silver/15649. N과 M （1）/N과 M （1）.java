70d3e88537eab7243e51abd8feb4963b6d952558
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-26 20:58:54 +0900

    [Silver III] Title: N과 M (1), Time: 220 ms, Memory: 23008 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,40 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[] arr;
+	static int n, m;
 	static StringBuilder sb = new StringBuilder();
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[M];
-		
-		func(0, 0);
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[m];
+		boolean[] visit = new boolean[10];
+		recur(0, arr, visit);
+
 		System.out.println(sb);
-		
 	}
-	
-	public static void func(int n, int bitmask) {
-		if(n == M) {
-			for(int i=0; i<M; i++) {
-				sb.append(arr[i] + " ");
-			}
+
+	public static void recur(int depth, int[] arr, boolean[] visit){
+		if(depth == m){
+			for(int a : arr)
+				sb.append(a).append(" ");
 			sb.append("\n");
 			return;
 		}
-		
-		for(int i=1; i<=N; i++) {
-			if((bitmask & (1 << (i - 1))) != 0) continue;
-			arr[n] = i;
-			func(n+1, bitmask | (1 << (i - 1)));
+
+		for(int i=1; i<=n; i++){
+			if(!visit[i]) {
+				arr[depth] = i;
+				visit[i] = true;
+				recur(depth + 1, arr, visit);
+				visit[i] = false;
+			}
 		}
 	}
 }
