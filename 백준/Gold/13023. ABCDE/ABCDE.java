617b40f86e13404f3a131ed20893b3d1d08e390d
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-27 09:16:38 +0900

    [Gold V] Title: ABCDE, Time: 272 ms, Memory: 19880 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static ArrayList<Integer>[] list;
+	static boolean[] visited;
+	static int answer = 0;
+	public static void main(String[] args) throws IOException{
+		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		  StringTokenizer st = new StringTokenizer(br.readLine());
+		  
+		  int N = Integer.parseInt(st.nextToken());
+		  int M = Integer.parseInt(st.nextToken());
+		  
+		  list = new ArrayList[N];
+		  for(int i=0; i<N; i++) {
+			  list[i] = new ArrayList<>();
+		  }
+		  
+		  for(int i=0; i<M; i++) {
+			  st = new StringTokenizer(br.readLine());
+			  int a = Integer.parseInt(st.nextToken());
+			  int b = Integer.parseInt(st.nextToken());
+			  list[a].add(b);
+			  list[b].add(a);
+		  }
+		  
+		  for(int i=0; i<N; i++) {
+			  visited = new boolean[N];
+			  dfs(i, 0);
+			  
+			  if(answer == 1)
+				  break;
+		  }
+		  
+		  System.out.println(answer);
+	}
+	
+	public static void dfs(int x, int len) {
+		if(len == 4) {
+			answer = 1;
+			return;
+		}
+		
+		visited[x] = true;
+		for(int i=0; i<list[x].size(); i++	) {
+			int temp = list[x].get(i);
+			if(visited[temp] == false) {
+				visited[temp] = true;
+				dfs(temp, len + 1);
+				visited[temp] = false;
+			}
+		}
+	}
+}
