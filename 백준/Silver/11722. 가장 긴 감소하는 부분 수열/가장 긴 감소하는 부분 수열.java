40ce9a3eeb19969a5fc6b2b600a3c0b0d2ec3588
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-10-30 23:57:00 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 128 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,30 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int A = Integer.parseInt(br.readLine());
-		int arr[] = new int[A+1];
-		int dp[] = new int[A+1];
-		
+
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n + 1];
 		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=A; i++)
+		for (int i = 1; i <= n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-		
-		int max = -1;
-		for(int i=1; i<=A; i++) {
-			dp[i] = 1;
-			for(int j=1; j<i; j++) {
-				if(arr[i]<arr[j] && dp[i]<=dp[j])
-					dp[i] = dp[j]+1;
-			}
-			max = Math.max(max, dp[i]);
 		}
-		
-		System.out.println(max);
+
+		int min = Integer.MIN_VALUE;
+		int[] dp = new int[n+1];
+		Arrays.fill(dp, 1);
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<i; j++){
+				if(arr[i] < arr[j]){
+					dp[i] = Math.max(dp[i], dp[j]+1);
+				}
+			}
+			min = Math.max(min, dp[i]);
+		}
+
+		System.out.println(min);
 	}
 }
