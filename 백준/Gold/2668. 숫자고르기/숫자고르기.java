b9902ba92501519b2cf2cecc5f9d834639122e7a
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-11-01 23:42:23 +0900

    [Gold V] Title: 숫자고르기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,21 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int n;
 	static int[] arr;
 	static boolean[] visit;
-	static List<Integer> list = new ArrayList<>();
+	static List<Integer> list;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
 		arr = new int[n+1];
 		visit = new boolean[n+1];
+		list = new ArrayList<>();
 
 		for(int i=1; i<=n; i++){
 			arr[i] = Integer.parseInt(br.readLine());
@@ -27,21 +26,22 @@ public class Main {
 			visit[i] = false;
 		}
 
+		sb.append(list.size()).append("\n");
 		Collections.sort(list);
-		System.out.println(list.size());
-		for(int x: list){
-			System.out.println(x);
+		for(int i : list){
+			sb.append(i).append("\n");
 		}
+
+		System.out.println(sb);
 	}
 
-	public static void dfs(int start, int target){
-		if(arr[start] == target)
-			list.add(target);
-
+	static void dfs(int start, int target){
 		if(!visit[arr[start]]){
 			visit[arr[start]] = true;
 			dfs(arr[start], target);
 			visit[arr[start]] = false;
 		}
+		if(arr[start] == target)
+			list.add(target);
 	}
 }
