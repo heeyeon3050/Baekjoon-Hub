bda0aea649041ebcfb5e4a915a6495899bcfaf20
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-11-02 09:30:10 +0900

    [Gold IV] Title: 플로이드, Time: 344 ms, Memory: 43424 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -4,55 +4,56 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+    static int INF = 100000 * 100;
 
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		int[][] arr = new int[n+1][n+1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		//초기값 설정
-		for(int i=1; i<=n; i++){
-			for(int j=1; j<=n; j++){
-				arr[i][j] = 9900001;
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-				if(i==j)
-					arr[i][j] = 0;
-			}
-		}
+        int[][] arr = new int[n+1][n+1];
 
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-            
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			
-			arr[a][b] = Math.min(arr[a][b], c);
-		}
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                arr[i][j] = INF;
 
-		for(int k=1; k<=n; k++){
-			for(int i=1; i<=n; i++){
-				for(int j=1; j<=n; j++){
-					if(arr[i][j] > arr[i][k] + arr[k][j]){
-						arr[i][j] = arr[i][k] + arr[k][j];
-					}
-				}
-			}
-		}
+                if(i == j){
+                    arr[i][j] = 0;
+                }
+            }
+        }
 
-		for(int i=1; i<=n; i++){
-			for(int j=1; j<=n; j++){
-				if(arr[i][j] == 9900001)
-					arr[i][j] = 0;
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-				sb.append(arr[i][j] + " ");
-			}
-			sb.append("\n");
-		}
+            arr[a][b] = Math.min(arr[a][b], c);
+        }
 
-		System.out.println(sb);
-	}
+        for(int k=1; k<=n; k++){
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=n; j++){
+                    if(arr[i][j] > arr[i][k] + arr[k][j])
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                }
+            }
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(arr[i][j] == INF)
+                    arr[i][j] = 0;
+
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
