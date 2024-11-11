5edbefb16db3619d36209bb2aa0451dc8d69cb66
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-11-12 00:03:17 +0900

    [Silver III] Title: 다음 순열, Time: 296 ms, Memory: 18996 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.java
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -1,56 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n;
 	static int[] arr;
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringTokenizer st;
+
 		int n = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+		st = new StringTokenizer(br.readLine());
 		arr = new int[n];
-		
-		for(int i=0; i<n; i++) {
+		for(int i=0; i<n; i++){
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		if (nextPermutation()) {
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
-            }
-        } else {
-            System.out.println("-1");
-        }
-		
-	}
-	
-	public static boolean nextPermutation() {
-		int i = arr.length - 1;
-		while(i > 0 && arr[i-1] >= arr[i]) i--;
-		if(i <= 0) return false;
-		
-		
-		int j = arr.length - 1;
-		while(arr[j] <= arr[i - 1]) j--;
-		
-		swap(i-1, j);
-		j = arr.length-1;
-		while(i < j) {
-			swap(i, j);
-			i++; j--;
+
+		if(!nextPermutation())
+			System.out.println(-1);
+		else {
+			for(int i : arr){
+				System.out.print(i + " ");
+			}
 		}
+	}
+
+	public static boolean nextPermutation(){
+		int i = arr.length - 1;
+		while(i > 0 && arr[i-1] >= arr[i])
+			i--;
+
+		if(i == 0)
+			return false;
+
+		int j = arr.length - 1;
+		while(arr[i-1] >= arr[j])
+			j--;
+
+		swap(i-1, j);
+
+		j = arr.length - 1;
+		while(i < j){
+			swap(i, j);
+			i++;
+			j--;
+		}
+
 		return true;
 	}
-	
-	public static void swap(int idx1, int idx2) {
-        int temp = arr[idx1];
-        arr[idx1] = arr[idx2];
-        arr[idx2] = temp;
-    }
+
+	public static void swap(int i, int j){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 }
