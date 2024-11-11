be9f718f8eeab6d6da40728fc6d4d22e52849233
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-11-12 00:55:23 +0900

    [Silver II] Title: 차이를 최대로, Time: 128 ms, Memory: 15240 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,48 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int n;
 	static int[] arr;
 	static boolean[] visit;
-	static int n;
 	static int max = Integer.MIN_VALUE;
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		n = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		
+
 		arr = new int[n];
 		visit = new boolean[n];
-		for(int i=0; i<n; i++) {
+
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++){
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		dfs(0, new int[n]);
+
+		recur(0, new int[n]);
+
 		System.out.println(max);
 	}
-	
-	public static void dfs(int L, int[] A) {
-		if(L == n) {
-			int answer = 0;
-			for(int i=0; i<n-1; i++) {
-				answer += Math.abs(A[i] - A[i+1]);
-			}
-			max = Math.max(answer, max);
+
+	public static void recur(int depth, int[] num){
+		if(depth == n){
+			max = Math.max(max, calculate(num));
 			return;
 		}
-		
-		for(int i=0; i<n; i++) {
+
+		for(int i=0; i<n; i++){
+			num[depth] = arr[i];
 			if(!visit[i]) {
 				visit[i] = true;
-				A[L] = arr[i];
-				dfs(L+1, A);
+				recur(depth + 1, num);
 				visit[i] = false;
 			}
 		}
+	}
+
+	public static int calculate(int[] arr){
+		int length = arr.length;
+		int sum = 0;
+
+		for(int i=0; i<length-1; i++){
+			sum += Math.abs(arr[i] - arr[i+1]);
+		}
+		return sum;
 	}
 }
