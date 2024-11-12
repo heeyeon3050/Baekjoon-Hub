6503ae8d0a0fae9c776b00b912605e7af9e65d48
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2024-11-12 12:11:22 +0900

    [Silver II] Title: 연속합, Time: 228 ms, Memory: 23812 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,28 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n+1];
-		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=n; i++){
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N+1];
+		int[] dp = new int[N+1];
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
-		int[] dp = new int[n+1];
-		for(int i=1; i<=n; i++){
-			dp[i] = Math.max(arr[i], dp[i-1]+arr[i]);
-		}
-
-		int max = Integer.MIN_VALUE;
-		for(int i=1; i<=n; i++){
+		
+ 
+		/*
+		 * dp[0]은 첫 원소로 이전에 더이상 탐색할 것이 없기 때문에
+		 * arr[0] 자체 값이 되므로 arr[0]으로 초기화 해준다.
+		 * max또한 첫 번째 원소로 초기화 해준다.
+		 */
+		dp[1] = arr[1];
+		int max = arr[1];
+		
+		for (int i = 2; i <= N; i++) {
+			// (이전 dp + 현재 arr값) 과 현재 arr값 중 큰 것을 dp에 저장 
+			dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+			// 최댓값 갱신 
 			max = Math.max(max, dp[i]);
 		}
-
 		System.out.println(max);
+ 
 	}
 }
